323748d62568d6454db731bbfb3dd56993563f18
Francisco Ribeiro <104844729+bakuryn@users.noreply.github.com>
2022-06-26 13:55:02 -0300

    Alterações iniciais
    index adicionado, arquivo de conexão excluído, php de salvar adicionado

---
+++ salvar.php
@@ -0,0 +1,48 @@
+<?php
+    @ini_set('discplay_errors', '1');
+    error_reporting(E_ALL);
+
+    $nome=$_POST["nome"];
+    $endereco=$_POST["endereco"];
+    $cnpj=$_POST["cnpj"];
+    $cidade=$_POST["cidade"];
+    $estado=$_POST["estado"];
+    $cep=$_POST["cep"];
+
+    $banco = mysqli_connect("localhost", "root", "");
+    mysqli_select_db($banco, "estoque");
+
+    $sql = "INSERT INTO fornecedor VALUES(DEFAULT, '$nome', '$endereco', '$cnpj', '$cidade', '$estado', '$cep')";
+
+    $resultado = mysqli_query($banco, $sql);
+
+    mysqli_close($banco);
+
+    if($resultado){
+        echo "Salvo com sucesso";
+    }else{
+        echo "Erro ao salvar os dados no Banco";
+    }
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Restaurante</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>          
+        
+    <!-- Botões-->
+    <div class="container">
+        <ul>
+            <li><a href="cad_fornecedor.html"><button>Voltar</button></a></li>
+        </ul>
+    </div> 
+    
+</body>
+</html>
