e65cff8a09b100da7cdd841124c9ffdf6f54292a
Francisco Ribeiro <104844729+bakuryn@users.noreply.github.com>
2022-06-26 14:32:06 -0300

    Update listar.php

--- listar.php
+++ listar.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Lista de Peças</title>
+    <title>Lista de Fornecedores</title>
     <link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
