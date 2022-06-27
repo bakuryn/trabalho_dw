f9c36f17fdb055ce44e6100865d9aa88884953d8
riquelmer45 <riquelmer_modesto@hotmail.com>
2022-06-27 12:56:24 -0300

    backand produtos

---
+++ EditarProduto.php
@@ -0,0 +1,81 @@
+<?php
+   <?php
+   if(!empty($_GET['id'])){
+       @ini_set('discplay_errors', '1');
+       error_reporting(E_ALL);
+
+       $id = $_GET["id"];        
+
+       $banco = mysqli_connect("localhost", "root", "");
+       mysqli_select_db($banco, "estoque");
+
+       $sql = "SELECT * FROM produto WHERE id=$id";
+
+       $resultado = mysqli_query($banco, $sql);
+
+       if($resultado->num_rows > 0){
+           while($user_data = mysqli_fetch_assoc($resultado)){
+            $idProduto = $user_data["id_produto"]
+            $idFornecedor = $user_data["id_fornecedor"]
+            $nome = $user_data["nomeProd"];
+            $peso = $user_data["peso"]
+            $precouni = $user_data["precoUnitario"];
+            $precotot = $user_data["precoTotal"];
+                            
+           }          
+       }
+       else{
+           header("Location:listar.php");
+       }
+
+       mysqli_close($banco);                
+   }
+
+   ?>
+
+
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Controle de Restaurante</title>
+   <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body> 
+   <div class="form">
+           <div class="t">
+               <h1>Cadastro de Produtos</h1>  
+           </div>
+           
+           <form action="salvarEditar.php" method="post">            
+               <!-- Form-->
+
+               <label for="">Nome</label>
+               <input type="text" name="nomeProd" size="10" maxlength="20" id="" value="<?php echo $nome?>" required>
+
+               <label for="">Peso</label>
+               <input type="text" name="peso" size="10" maxlength="10" id="" value="<?php echo $peso ?>" required>
+
+               <label for="">Preço Unitário</label>
+               <input type="text" name="precoUnitario" size="10" maxlength="10" id="" value="<?php echo $precouni?>" required>
+
+               <label for="">Preço total</label>
+               <input type="text" name="precoTotal" size="10" maxlength="10" id="" value="<?php echo $precotot?>" required>
+
+
+               <!-- Botões-->
+               <div class="bottom">
+                   <input type="hidden" name="id" value="<?php echo $id ?>">
+                   <input type="submit" name="update" id="update" value="Salvar">
+                   <input type="reset" value="Limpar">                
+               </div>                   
+           </form>
+           <a href="listar.php"><button class="voltar">Voltar</button></a>
+       </div> 
+   
+</body>
+</html>
+?>
---
+++ ListarProduto.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lista de Produtos</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+    <?php
+        @ini_set('discplay_errors', '1');
+        error_reporting(E_ALL);
+
+        $banco = mysqli_connect("localhost", "root", "");
+        mysqli_select_db($banco, "estoque");
+
+        $sql = "select * from produto";
+
+        $resultado = mysqli_query($banco, $sql);
+
+        mysqli_close($banco);
+    ?>
+
+    <?php
+        if(mysqli_num_rows($resultado) < 1){
+            exit;
+        }
+    ?>
+
+    <table width="50%" border ="1" align="center">
+        <tr>
+            <th>ID</th>
+            <th>ID_Fornecedor</th>
+            <th>Nome</th>
+            <th>Peso</th>
+            <th>Preço Unitário</th>
+            <th>Preço Total</th>
+            
+        </tr>
+        <?php
+            while($l = mysqli_fetch_array($resultado)){
+                $id = $l["id_produto"];
+                $ID_fornecedor = $l["id_fornecedor"];
+                $nome = $l["nomeProd"];
+                $peso = $l["peso"];
+                $precouni = $l["precoUnitario"];
+                $precotot = $l["precoTotal"];
+                
+
+                echo"
+                <tr>
+                    <td>$id</td>
+                    <td>$ID_Fornecedor</td>
+                    <td>$Nome</td>
+                    <td>$peso</td>
+                    <td>$precouni</td>
+                    <td>$precotot</td>
+                    <td>
+                        <a href=\"editar.php?id=$id\">[Editar]</a>
+                        <a href=\"excluir.php?id=$id\">[Excluir]</a>
+                    </td>
+
+                </tr>\n";
+            }
+        ?>
+    </table>
+    <!-- Botões-->
+    <div class="container">
+        <ul>
+            <li><a href="index.html"><button>Voltar</button></a></li>
+        </ul>
+    </div>  
+</body>
+</html>
